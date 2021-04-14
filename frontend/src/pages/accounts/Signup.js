f0fb37084ee97d8fd9ba67cf9de4f4bb49b79d0c
noname2048 <sungwook.csw@gmail.com>
2021-04-14 23:35:35 +0900

    어렵다.. 회원가입 양식 antd 디자인 적용 밑 로직 구성

--- frontend/src/pages/accounts/Signup.js
+++ frontend/src/pages/accounts/Signup.js
@@ -1,71 +1,107 @@
 import React, { useEffect, useState } from "react";
-import { Alert } from "antd";
+import { Form, Input, Button, notification } from "antd";
 import Axios from "axios";
 import { useHistory } from "react-router-dom";
+import { FrownOutlined, SmileOutlined } from "@ant-design/icons";
 
 export default function Signup() {
   const history = useHistory();
+  const [fieldErrors, setFieldErrors] = useState({});
 
-  const [inputs, setInputs] = useState({ username: "", password: "" });
-  const [loading, setLoading] = useState(false);
-  const [errors, setErrors] = useState({});
-  const [formDisabled, setFormDisabled] = useState(true);
+  const onFinish = (values) => {
+    async function fn() {
+      const { username, password } = values;
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+      setFieldErrors({});
 
-    setErrors({});
+      const data = { username, password };
+      try {
+        const response = await Axios.post(
+          "http://localhost:8000/accounts/signup/",
+          data
+        );
 
-    Axios.post("http://localhost:8000/accounts/signup/", inputs)
-      .then((response) => {
-        console.log("response: ", response);
+        notification.open({
+          message: "회원가입 성공",
+          description: "로그인 페이지로 이동합니다.",
+          icon: <SmileOutlined style={{ color: "#108ee9" }} />,
+        });
+
         history.push("/accounts/login");
-      })
-      .catch((error) => {
-        console.log("error: ", error);
+      } catch (error) {
         if (error.response) {
-          setErrors({
-            username: (error.response.data.username || []).join(" "),
-            password: (error.response.data.password || []).join(" "),
+          notification.open({
+            message: "회원가입 실패",
+            description: "아이디/암호를 확인해주세요.",
+            icon: <FrownOutlined style={{ color: "#ff3333" }} />,
           });
+
+          const { data: fieldsErrorMessages } = error.response;
+          // fieldsErrorMessages => {username: ["n1", "n2"], password: []}
+          setFieldErrors(
+            Object.entries(fieldsErrorMessages).reduce(
+              (acc, [fieldName, errors]) => {
+                // errors: ["m1", "m2"].jsoin("")
+                acc[fieldName] = {
+                  validateStatus: "error",
+                  help: errors.join(" "),
+                };
+                return acc;
+              },
+              {}
+            )
+          );
         }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+      }
+    }
+    fn();
 
-  useEffect(() => {
-    const isEnabled = Object.values(inputs).every((s) => s.length > 0);
-    setFormDisabled(!isEnabled);
-  }, [inputs]);
-
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    console.log("onFinish: ", values);
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit} action="">
-        <div>
-          <input type="text" name="username" onChange={onChange} />
-          {errors.username && <Alert type="error" message={errors.username} />}
-        </div>
-        <div>
-          <input type="password" name="password" onChange={onChange} />
-          {errors.password && <Alert type="error" message={errors.password} />}
-        </div>
+    <Form
+      {...layout}
+      onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          { required: true, message: "Please input your username!" },
+          { min: 5, message: "5글자 이상 입력해주세요." },
+        ]}
+        hasFeedback
+        {...fieldErrors.username}
+      >
+        <Input />
+      </Form.Item>
 
-        <input
-          type="submit"
-          value="회원가입"
-          disabled={loading || formDisabled}
-        />
-      </form>
-    </div>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+        hasFeedback
+        {...fieldErrors.password}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
