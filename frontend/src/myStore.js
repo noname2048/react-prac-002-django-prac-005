e3061d20eeb914e7c0b3ad1b58c0b50722265cb1
noname2048 <sungwook.csw@gmail.com>
2021-04-22 17:19:00 +0900

    myStore 컴포넌트에 isAuthenticate 와 주석 추가

--- frontend/src/myStore.js
+++ frontend/src/myStore.js
@@ -4,20 +4,35 @@ export const SET_TOKEN = "MYSTORE/SET_TOKEN";
 export const DELETE_TOKEN = "MYSTORE/DELETE_TOKEN";
 const TOKEN_KEY_NAME = "userJwtToken";
 
+const publicActions = {
+  SET_TOKEN: "MYSTORE/SET_TOKEN",
+  DELETE_TOKEN: "MYSTORE/DELETE_TOKEN",
+};
+
+const pricateActions = {
+  INIT_TOKEN: "MYSTORE/INIT_TOKEN",
+};
+
 function reducer(prevState, action) {
-  console.log("스토어 리듀서");
+  console.log("LOG: MYSTORE/REDUCER");
 
   switch (action.type) {
-    case SET_TOKEN: {
+    case publicActions.SET_TOKEN: {
       return {
         ...prevState,
         jwtToken: action.payload.jwtToken,
       };
     }
-    case DELETE_TOKEN: {
+    case publicActions.DELETE_TOKEN: {
       return {
         ...prevState,
         jwtToken: "",
+      };
+    }
+    case pricateActions.INIT_TOKEN: {
+      return {
+        ...prevState,
+        jwtToken: action.payload.jwtToken,
       };
     }
     default: {
@@ -26,44 +41,56 @@ function reducer(prevState, action) {
   }
 }
 
+// 아래 MyStoreProvider에 쓰이는 Context 객체
 export const MyStoreContext = createContext();
 
+/**
+ * state에 3개를 담고 있는 Provider 내장 REACT 컴포넌트
+ *
+ * <MyStoreProvider> <div> ... </div> </MyStoreProvider> 로 정의하면
+ * Provider가 자동 적용되어 useContext로 state를 사용 할 수 있다.
+ */
 export function MyStoreProvider({ children }) {
-  let initValueJwtToken = "";
+  let dumyValue = 3;
+  let jwtToken = "";
+  let isAuthentiacted = false;
 
   try {
-    initValueJwtToken = window.localStorage.getItem(TOKEN_KEY_NAME);
+    jwtToken = window.localStorage.getItem(TOKEN_KEY_NAME);
   } catch {
-    initValueJwtToken = "";
+    jwtToken = "";
+  }
+
+  if (jwtToken.length > 0) {
+    isAuthentiacted = true;
   }
 
   const initialValues = {
-    version: 3,
-    jwtToken: initValueJwtToken,
+    version: dumyValue,
+    jwtToken,
+    isAuthentiacted,
   };
 
   const [state, dispatch] = useReducer(reducer, initialValues);
 
+  // 단순히 마운트시 정보만 표기
   useEffect(() => {
-    console.log("초기 데이터", initialValues);
+    console.log("LOG: MYSTORE/MOUNT", state);
   }, []);
 
+  // state 중 jwtToken이 변한 경우, 그 값만 localStorage에 저장한다.
   useEffect(() => {
-    console.log("유즈 이펙트");
-
     try {
-      console.log("단계 H");
       const { jwtToken } = state;
-
-      console.log("jwtToken", jwtToken);
       window.localStorage.setItem(TOKEN_KEY_NAME, JSON.stringify(jwtToken));
     } catch (error) {
       console.log(error);
     }
+    console.log("LOG: MYSTORE/UDPATE", state);
   }, [state]);
 
   return (
-    <MyStoreContext.Provider value={{ dispatch }}>
+    <MyStoreContext.Provider value={{ state, dispatch }}>
       {children}
     </MyStoreContext.Provider>
   );
