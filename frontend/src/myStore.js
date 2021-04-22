77fdeecc410d1856583c6d95d5e368a4ef7ea5ba
noname2048 <sungwook.csw@gmail.com>
2021-04-22 18:28:08 +0900

    myStore isAutenticated 삽입 및 리팩토링

--- frontend/src/myStore.js
+++ frontend/src/myStore.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 
 export const SET_TOKEN = "MYSTORE/SET_TOKEN";
 export const DELETE_TOKEN = "MYSTORE/DELETE_TOKEN";
@@ -7,32 +7,42 @@ const TOKEN_KEY_NAME = "userJwtToken";
 const publicActions = {
   SET_TOKEN: "MYSTORE/SET_TOKEN",
   DELETE_TOKEN: "MYSTORE/DELETE_TOKEN",
+  GET_TOKEN: "MYSTORE/GET_TOKEN",
 };
 
-const pricateActions = {
-  INIT_TOKEN: "MYSTORE/INIT_TOKEN",
-};
+const pricateActions = {};
 
 function reducer(prevState, action) {
   console.log("LOG: MYSTORE/REDUCER");
 
   switch (action.type) {
     case publicActions.SET_TOKEN: {
+      const { jwtToken } = action.payload;
       return {
         ...prevState,
-        jwtToken: action.payload.jwtToken,
+        jwtToken: jwtToken,
+        isAuthentiacted: jwtToken.length > 0 ? true : false,
       };
     }
     case publicActions.DELETE_TOKEN: {
       return {
         ...prevState,
         jwtToken: "",
+        isAuthentiacted: false,
       };
     }
-    case pricateActions.INIT_TOKEN: {
+    case publicActions.GET_TOKEN: {
+      let jwtToken = null;
+
+      try {
+        jwtToken = JSON.parse(window.localStorage.getItem(TOKEN_KEY_NAME));
+      } catch (error) {
+        console.log(error);
+      }
+
       return {
         ...prevState,
-        jwtToken: action.payload.jwtToken,
+        jwtToken,
       };
     }
     default: {
@@ -56,12 +66,12 @@ export function MyStoreProvider({ children }) {
   let isAuthentiacted = false;
 
   try {
-    jwtToken = window.localStorage.getItem(TOKEN_KEY_NAME);
+    jwtToken = JSON.parse(window.localStorage.getItem(TOKEN_KEY_NAME));
   } catch {
     jwtToken = "";
   }
 
-  if (jwtToken.length > 0) {
+  if (jwtToken != null && jwtToken.length > 0) {
     isAuthentiacted = true;
   }
 
@@ -78,11 +88,15 @@ export function MyStoreProvider({ children }) {
     console.log("LOG: MYSTORE/MOUNT", state);
   }, []);
 
-  // state 중 jwtToken이 변한 경우, 그 값만 localStorage에 저장한다.
+  // state 중 jwtToken이 변할 경우, 비동기적으로 그 값만 localStorage에 저장한다.
   useEffect(() => {
     try {
       const { jwtToken } = state;
-      window.localStorage.setItem(TOKEN_KEY_NAME, JSON.stringify(jwtToken));
+      if (jwtToken === "" || jwtToken === null) {
+        window.localStorage.setItem(TOKEN_KEY_NAME, JSON.stringify(null));
+      } else {
+        window.localStorage.setItem(TOKEN_KEY_NAME, JSON.stringify(jwtToken));
+      }
     } catch (error) {
       console.log(error);
     }
