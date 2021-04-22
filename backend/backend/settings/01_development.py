922315ae8185bb2782fbd94c79b4f19d80fc41bd
noname2048 <sungwook.csw@gmail.com>
2021-04-23 02:31:16 +0900

    JWT EXPIRATIONI_DELTA 가 300초에서 3일로 변경

--- backend/backend/settings/01_development.py
+++ backend/backend/settings/01_development.py
@@ -161,7 +161,7 @@ JWT_AUTH = {
     "JWT_SECRET_KEY": SECRET_KEY,
     "JWT_ALGORITHM": "HS256",
     "JWT_ALLOW_REFRESH": True,
-    "JWT_EXPIRATION_DELTA": datetime.timedelta(seconds=300),
+    "JWT_EXPIRATION_DELTA": datetime.timedelta(days=1),
     "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=7),
 }
 
