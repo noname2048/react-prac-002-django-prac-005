0061de56a54d2cbcee4a813c1c596d6053fa7437
noname2048 <sungwook.csw@gmail.com>
2021-04-23 02:32:08 +0900

    react postlist 요청시 이제 jwt를 넣어서 요청

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Axios from "axios";
 import Post from "components/Post";
-
+import { MyStoreContext } from "myStore"
+import { Alert } from "antd";
 const apiUrl = "http://localhost:8000/instagram/api/posts/";
 
 function PostList() {
+  const { state: { jwtToken } } = useContext(MyStoreContext);
   const [postList, setPostList] = useState([]);
 
   useEffect(() => {
-    Axios.get(apiUrl)
+    console.log("LOG :PostList jwtToken", jwtToken);
+    const headers = { Authorization: `JWT ${jwtToken}`};
+    Axios.get(apiUrl, {headers})
       .then((response) => {
         const { data } = response;
         setPostList(data);
@@ -23,9 +27,10 @@ function PostList() {
 
   return (
     <div>
-      {postList.map((post) => (
+      { postList.length === 0 && <Alert type="warning" message="포스팅이 없습니다. : -(" /> }
+      { postList.map((post) => (
         <Post post={post} key={post.id} />
-      ))}
+      )) }
     </div>
   );
 }
