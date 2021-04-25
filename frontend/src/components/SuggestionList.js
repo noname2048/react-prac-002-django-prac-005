2017ee1a15259517b149bc219e2e37900050c7d8
noname2048 <sungwook.csw@gmail.com>
2021-04-25 19:41:47 +0900

    apply useAxios hooks to SuggestionList

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -4,40 +4,49 @@ import Suggestion from "components/Suggestion";
 import "components/SuggestionList.scss";
 import Axios from "axios";
 import { MyStoreContext } from "myStore";
+import useAxios from "axios-hooks";
 
 export default function SuggestionList({ style }) {
-  const [userList, setUserList] = useState([]);
   const { state } = useContext(MyStoreContext);
+  const headers = { Authorization: `JWT ${state.jwtToken}` };
+  const [{ data: userList, loading, error }, refetch] = useAxios({
+    url: "http://localhost:8000/accounts/suggestions/",
+    headers,
+  });
 
-  useEffect(() => {
-    async function fetchUserList() {
-      const apiUrl = "http://localhost:8000/accounts/suggestions/";
-      const headers = { Authorization: `JWT ${state.jwtToken}` };
+  // const [userList, setUserList] = useState([]);
+  // const { state } = useContext(MyStoreContext);
 
-      try {
-        const response = await Axios.get(apiUrl, { headers });
-        setUserList(response.data);
-        console.log("response: ", response);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchUserList();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchUserList() {
+  //     const apiUrl = "http://localhost:8000/accounts/suggestions/";
+  //     const headers = { Authorization: `JWT ${state.jwtToken}` };
+
+  //     try {
+  //       const response = await Axios.get(apiUrl, { headers });
+  //       setUserList(response.data);
+  //       console.log("response: ", response);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   fetchUserList();
+  // }, []);
 
   return (
-    <div>
+    <div style={style}>
+      {loading && <div>Loading ... </div>}
+      {error && <div>로딩 중에 에러가 발생했습니다.</div>}
+
+      <button onClick={() => refetch()}>Reload</button>
       <Card title="Suggestions for you" size="small">
-        {userList.map((suggestionUser) => (
-          <Suggestion
-            key={suggestionUser.username}
-            suggestionUser={suggestionUser}
-          />
-        ))}
-        {/* <Suggestion />
-        <Suggestion />
-        <Suggestion />
-        <Suggestion /> */}
+        {userList &&
+          userList.map((suggestionUser) => (
+            <Suggestion
+              key={suggestionUser.username}
+              suggestionUser={suggestionUser}
+            />
+          ))}
       </Card>
     </div>
   );
