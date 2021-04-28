4177b4566f18a1bbe435f3d148b6902a1dc11d47
noname2048 <sungwook.csw@gmail.com>
2021-04-28 21:37:06 +0900

    post serializer에 대한 context에 request 추가, like_user 확인 관련 함수 추가

--- backend/instagram/serializers.py
+++ backend/instagram/serializers.py
@@ -32,6 +32,24 @@ class UserSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
 
+    is_like = serializers.SerializerMethodField("is_like_field")
+
+    def is_like_field(self, post):
+        if "request" in self.context:
+            user = self.context["request"].user
+            return post.like_user_set.filter(pk=user.pk).exists()
+
+        return False
+
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = [
+            "id",
+            "user",
+            "created_at",
+            "photo",
+            "caption",
+            "location",
+            "tag_set",
+            "is_like",
+        ]
