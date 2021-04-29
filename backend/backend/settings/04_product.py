ff69514e31370afd901b4e1ca1d7f7f1f3d80513
noname2048 <sungwook.csw@gmail.com>
2021-04-30 01:13:45 +0900

    postgresql 데이터베이스 추가, env incapsulation 화

--- backend/backend/settings/04_product.py
+++ backend/backend/settings/04_product.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "USER": os.environ.get("DB_USER"),
+        "NAME": os.environ.get("DB_NAME"),
     }
 }
 
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.01_development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.04_product")
 
 application = get_wsgi_application()
